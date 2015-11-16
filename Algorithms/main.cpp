088e894278c71bb0bf7479be0d12dff98dd65080
Simon Dimitriadis <pumba6@gmx.at>
2015-11-16 02:32:03 +0100

    transfer

--- Algorithms/main.cpp
+++ Algorithms/main.cpp
@@ -5,10 +5,10 @@
 #include <algorithm>
 #include "PerformanceMonitor.h"
 #include "QuickSort.h"
-#include <immintrin.h>
 
 const bool readNumbersFromFile = false;
 
+using namespace std;
 
 enum Status {
 	STATUS_SUCCESS,
@@ -163,7 +163,7 @@ void selectionByMedianOfMedians(const vector<int> values, int k){
 
 	//    Calculating a new pivot for making splits
 	int m = findMedianOfMedians(vec2D);
-	cout << "Median of medians is : " << m << endl;
+	std::cout << "Median of medians is : " << m << endl;
 
 	//    Partition the list into unique elements larger than 'm' (call this sublist L1) and
 	//    those smaller them 'm' (call this sublist L2)
@@ -193,7 +193,7 @@ void selectionByMedianOfMedians(const vector<int> values, int k){
 
 	//    Recursive calls
 	if ((k - 1) == L1.size()) {
-		cout << endl << endl << "Answer :" << m;
+		std::cout << endl << endl << "Answer :" << m;
 	}
 	else if (k <= L1.size()) {
 		return selectionByMedianOfMedians(L1, k);
@@ -243,8 +243,6 @@ int select(int *a, int s, int e, int k){
 
 
 
-
-
 // nth element
 
 // This algorithm uses the std::nth_element function to rearrange the elements around the pivot
@@ -258,7 +256,7 @@ void std_nthElementSort(std::vector<int>& input, int left, int pivot, int right)
 	std::nth_element(input.begin() + left, input.begin() + pivot, input.begin() + right);	// do the "sort"
 
 	int newRange = pivot - left;
-	if (newRange < 3){ return; } // stop condition
+	if (newRange <= 3){ return; } // stop condition
 
 	int pivotOffset = newRange / 2;
 
@@ -273,40 +271,158 @@ void std_nthElementSort(std::vector<int>& input, int left, int pivot, int right)
 
 
 
+int findMedianOfMedians(std::vector<int>& input, int left, int right)
+{
+	std::vector<int> medianVecs;
+
+	int range = right - left;
+
+	for (int rangeCounter = left; rangeCounter < right;)
+	{
+		std::vector<int> fiveElem;
+		for (int i = 0; i < 5 && rangeCounter < right; ++i) /// probably switch to sort directly in the inputvector this could help to reduce memory allocs.. 
+		{
+			std::cout << input[left + rangeCounter] << " ";
+			fiveElem.push_back(input[left + rangeCounter]);
+			++rangeCounter;
+		}
+		std::sort(fiveElem.begin(), fiveElem.end());
+		std::cout << std::endl << "m: " << fiveElem[static_cast<int> (fiveElem.size() / 2)] << std::endl;
+
+		medianVecs.push_back(fiveElem[static_cast<int> (fiveElem.size() / 2)]);
+	}
+	std::cout << "---------------------------" << std::endl;
+	if (medianVecs.size() <= 5)
+	{
+		return medianVecs[medianVecs.size() / 2];
+	}
+	else
+	{
+		return findMedianOfMedians(medianVecs, 0, medianVecs.size());
+	}
+}
+
+// partition easy .. 
+int partition_around_pivot_a(std::vector<int>& input, int left, int right, int pivot)
+{
+
+	std::vector<int> lower;
+	std::vector<int> higher;
+	int pivotIdx = 0;
+	for (int i = left; i < right; ++i)
+	{
+		if (input[i] < pivot)
+		{
+			lower.push_back(input[i]);
+		}
+		if (input[i] > pivot)
+		{
+			higher.push_back(input[i]);
+		}
+		if (input[i] == pivot)
+		{
+			pivotIdx = i;
+		}
+
+	}
+	int count = left;
+	lower.push_back(input[pivotIdx]);
+	for (int i = 0; i < lower.size(); ++i)
+	{
+
+		input[count] = lower[i];
+		++count;
+	}
+
+	for (int i = 0; i < higher.size(); ++i)
+	{
+		input[count] = higher[i];
+		++count;
+	}
+	return lower.size();
+}
+
+
+//The Dselect Algo
+// break A into groups of 5 sort each group with what ever wanted
+// C = the n/5 "middle elements"
+// p = Select (C, n/5 , n/10) recursevly computes median of C
+// Partition A around p
+// if j = i return p
+// if j < i return select ( first part of A, j-1 , i)
+// else if j  > i return select ( second part of A , n-j , i-j)
+
+void DselectAlgo(std::vector<int>& input, int left, int right)
+{
+	std::cout << "---------------------------" << std::endl;
+
+	std::cout << left << "|" << right << std::endl;
+	std::vector<int>::iterator bound;
+
+	int median = findMedianOfMedians(input, left, right);
+
+	int indexPivot =  partition_around_pivot_a(input, left, right, median);
+
+	// std::partition(input.begin() + left, input.begin() + right, [median](int val){return val <= median; });
+
+	for (int i = 0; i < input.size(); ++i)
+	{
+		std::cout << input[i] << " ";
+	}
+	std::cout << std::endl;
+
+	int boundIdx = left + indexPivot;
+	//int boundIdx = bound - input.begin();
+
+	std::cout << "m: " << median;
+	std::cout << " idx " << boundIdx << std::endl;
+
+	// check if work is done
+	if ((boundIdx - left) > 2) // left side
+	{
+		std::cout << "left ";
+		DselectAlgo(input, left, boundIdx);
+	}
+	if ((right - boundIdx) > 2) // right side
+	{
+		std::cout << "right ";
+		DselectAlgo(input, boundIdx + 1, right);
+	}
+
+}
+
+
+
+
+
 
 
 
 int main() {
 
-	//std::vector<int> testVec; // = { 51, 4, 1, 5, 9, 0, 10, 3, 2, 8, 117, 237, 32, 162, 367, 129, 50, 417, 34, 161, 429, 118, 235, 252, 427, 4, 480, 242, 410, 258, 374, 368, 459, 89, 77, 106 };
-	//int size = 360;
-	//for (int i = 0; i < size; ++i)
-	//{
-	//	testVec.push_back(utils::randRangeRD(0, 500));
-	//	std::cout << testVec[i] << " ";
-	//}
-	//std::cout << std::endl;
 
-	//std::cout << *(testVec.begin() + 1) << std::endl;
-	//std::cout << *(testVec.begin() + 2) << std::endl;
-	//std::cout << *(testVec.begin() + 3) << std::endl;
+	std::vector<int> testVec = { 1, 0, 51, 4, 5, 9 };
+	//std::vector<int> testVec = { 51, 4, 1, 5, 9, 0, 10, 3, 2, 8, 117 };
+	DselectAlgo(testVec, 0, testVec.size());
 
 
 
-	//std_nthElementSort(testVec, 0, testVec.size() / 2, testVec.size());
+	//int median = 1;
+	//std::vector<int>::iterator bound;
+	//utils::partition_around_pivot(&testVec[0], 0, 1, 0);
 
-	////std::nth_element(testVec.begin(), testVec.begin() + 1, testVec.begin() + 3);
+	//bound = std::partition(testVec.begin(), testVec.begin() + 2, [median](int val){return val <= median; });
 
+	//// print out content:
+	//std::cout << "front elements:";
+	//for (std::vector<int>::iterator it = testVec.begin(); it != bound; ++it)
+	//	std::cout << ' ' << *it;
+	//std::cout << '\n';
 
-	//for (int i = 0; i < size; ++i)
-	//{
-	//	std::cout << testVec[i] << " ";
-	//}
-	//std::cout << std::endl;
-	//std::cout << std::endl;
-	//checkSortResult(testVec);
-
-	//testVec.clear();
+	//std::cout << "from median elements:";
+	//for (std::vector<int>::iterator it = bound; it != testVec.end(); ++it)
+	//	std::cout << ' ' << *it;
+	//std::cout << '\n';
 
 
 
@@ -315,14 +431,40 @@ int main() {
 
 
 
-	//The Dselect Algo
-	// break A into groups of 5 sort each group with what ever wanted
-	// C = the n/5 "middle elements"
-	// p = Select (C, n/5 , n/10) recursevly computes median of C
-	// Partition A around p
-	// if j = i return p
-	// if j< i return select ( first part of A, j-1 , i)
-	// else if j  > i return select ( second part of A , n-j , i-j)
+
+	//std::vector<int> testVec = { 51, 4, 1, 5, 9, 0, 10, 3, 2, 8, 117, 237, 32, 162, 367, 129, 50, 417, 34, 161, 429, 118, 235, 252, 427, 4, 480, 242, 410, 258, 374, 368, 459, 89, 77, 106 };
+	int size = testVec.size();
+	for (int i = 0; i < size; ++i)
+	{
+		//testVec.push_back(utils::randRangeRD(0, 500));
+		std::cout << testVec[i] << " ";
+	}
+	std::cout << std::endl;
+
+	DselectAlgo(testVec, 0, testVec.size());
+	//std::nth_element(testVec.begin(), testVec.begin() + 1, testVec.begin() + 3);
+	checkSortResult(testVec);
+
+
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+
+	for (int i = 0; i < size; ++i)
+	{
+		std::cout << testVec[i] << " ";
+	}
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	testVec.clear();
+
+
+
+
+
+
+
 
 
 
@@ -352,7 +494,7 @@ int main() {
 		std::random_shuffle(originalNumbers.begin(), originalNumbers.end());
 	}
 	else {
-		originalNumbers = generateRandomNumbers(1000000, -10000, +10000);
+		originalNumbers = generateRandomNumbers(10000000, -10000, +10000);
 	}
 
 	// The buffers are needed later for better test case comparisons
@@ -377,27 +519,27 @@ int main() {
 
 	std::vector<int> numbers;
 
-	//////// stdSort: unsorted array
-	//////std::cout << "Running std::sort on unsorted array..." << std::endl;
-	//////numbers = originalNumbers;
-	//////monitor.start();
-	//////std::sort(numbers.begin(), numbers.end());
-	//////stdSort[0] = monitor.stop();
-	//////checkSortResult(numbers);
-	//////// stdSort: sorted array
-	//////std::cout << "Running std::sort on sorted array..." << std::endl;
-	//////numbers = sortedNumbers;
-	//////monitor.start();
-	//////std::sort(numbers.begin(), numbers.end());
-	//////stdSort[1] = monitor.stop();
-	//////checkSortResult(numbers);
-	//////// stdSort: reverse sorted array
-	//////std::cout << "Running std::sort on reverse sorted array..." << std::endl;
-	//////numbers = reversedNumbers;
-	//////monitor.start();
-	//////std::sort(numbers.begin(), numbers.end());
-	//////stdSort[2] = monitor.stop();
-	//////checkSortResult(numbers);
+	// stdSort: unsorted array
+	std::cout << "Running std::sort on unsorted array..." << std::endl;
+	numbers = originalNumbers;
+	monitor.start();
+	std::sort(numbers.begin(), numbers.end());
+	stdSort[0] = monitor.stop();
+	checkSortResult(numbers);
+	// stdSort: sorted array
+	std::cout << "Running std::sort on sorted array..." << std::endl;
+	numbers = sortedNumbers;
+	monitor.start();
+	std::sort(numbers.begin(), numbers.end());
+	stdSort[1] = monitor.stop();
+	checkSortResult(numbers);
+	// stdSort: reverse sorted array
+	std::cout << "Running std::sort on reverse sorted array..." << std::endl;
+	numbers = reversedNumbers;
+	monitor.start();
+	std::sort(numbers.begin(), numbers.end());
+	stdSort[2] = monitor.stop();
+	checkSortResult(numbers);
 
 
 
@@ -409,27 +551,27 @@ int main() {
 	//checkSortResult(numbers);
 
 
-	//////// quickSort: unsorted array
-	//////std::cout << "Running quickSort on unsorted array..." << std::endl;
-	//////numbers = originalNumbers;
-	//////monitor.start();
-	//////utils::quickSort(&(numbers[0]), numbers.size());
-	//////quickSort[0] = monitor.stop();
-	//////checkSortResult(numbers);
-	//////// quickSort: sorted array
-	//////std::cout << "Running quickSort on sorted array..." << std::endl;
-	//////numbers = sortedNumbers;
-	//////monitor.start();
-	//////utils::quickSort(&(numbers[0]), numbers.size());
-	//////quickSort[1] = monitor.stop();
-	//////checkSortResult(numbers);
-	//////// quickSort: reverse sorted array
-	//////std::cout << "Running quickSort on reverse sorted array..." << std::endl;
-	//////numbers = reversedNumbers;
-	//////monitor.start();
-	//////utils::quickSort(&(numbers[0]), numbers.size());
-	//////quickSort[2] = monitor.stop();
-	//checkSortResult(numbers);
+	// quickSort: unsorted array
+	std::cout << "Running quickSort on unsorted array..." << std::endl;
+	numbers = originalNumbers;
+	monitor.start();
+	utils::quickSort(&(numbers[0]), numbers.size());
+	quickSort[0] = monitor.stop();
+	checkSortResult(numbers);
+	// quickSort: sorted array
+	std::cout << "Running quickSort on sorted array..." << std::endl;
+	numbers = sortedNumbers;
+	monitor.start();
+	utils::quickSort(&(numbers[0]), numbers.size());
+	quickSort[1] = monitor.stop();
+	checkSortResult(numbers);
+	// quickSort: reverse sorted array
+	std::cout << "Running quickSort on reverse sorted array..." << std::endl;
+	numbers = reversedNumbers;
+	monitor.start();
+	utils::quickSort(&(numbers[0]), numbers.size());
+	quickSort[2] = monitor.stop();
+	checkSortResult(numbers);
 
 
 
@@ -509,14 +651,14 @@ int main() {
 		<< "-------------------------------------------------------------------------" << std::endl
 		<< "  std::sort   " << PerformanceMonitor::millisToString(stdSort[0]) << "        " << PerformanceMonitor::millisToString(stdSort[1]) << "         " << PerformanceMonitor::millisToString(stdSort[2]) << std::endl
 		<< "  quicksort   " << PerformanceMonitor::millisToString(quickSort[0]) << "        " << PerformanceMonitor::millisToString(quickSort[1]) << "         " << PerformanceMonitor::millisToString(quickSort[2]) << std::endl
-		
-		
+
+
 		<< " std::nthElem " << PerformanceMonitor::millisToString(stdNthElement[0]) << "        " << PerformanceMonitor::millisToString(stdNthElement[1]) << "         " << PerformanceMonitor::millisToString(stdNthElement[2]) << std::endl
 		<< " nth-element  " << PerformanceMonitor::millisToString(nthElement[0]) << "        " << PerformanceMonitor::millisToString(nthElement[1]) << "         " << PerformanceMonitor::millisToString(nthElement[2]) << std::endl;
 
 	std::cout << "Note: nth element has been executed " << sampleCount << " times per measurement." << std::endl;
 	int mid = numbers.size() / 2 + 5;
-	std::cout << "Median: Middle element has value " << sortedNumbers[mid] << std::endl;
+	//std::cout << "Median: Middle element has value " << sortedNumbers[mid] << std::endl;
 
 	std::cin.ignore();
 }
